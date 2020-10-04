7b2eea2f3028138300f8168e842ddb36289b9ad8
zhouzhihui <peregrinator@yeah.net>
2020-10-04 11:49:06 +0800

    chore(apiurl.js): replace the online API

--- config/apiUrl.ts
+++ config/apiUrl.ts
@@ -4,7 +4,7 @@ const HOST = process.env.NODE_ENV
 if (HOST === 'development') {
   ipUrl = 'http://127.0.0.1:7001/default/'
 } else if (HOST === 'production') {
-  ipUrl = 'http://47.105.40.202:7001/default/'
+  ipUrl = 'https://api.mynetdisk.top/default/'
 }
 const servicePath = {
   getArticleList: `${ipUrl}getArticleList`, // 首页接口
