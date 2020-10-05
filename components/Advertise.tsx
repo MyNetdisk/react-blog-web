eeb435066085a5696872130334dfc5b4ffcd9197
zhouzhihui <peregrinator@yeah.net>
2020-10-05 09:46:25 +0800

    chore(advertise.tsx): change advertise.tsx picture from http to https

--- components/Advertise.tsx
+++ components/Advertise.tsx
@@ -6,10 +6,26 @@ const Advertise = () => {
   return (
     <div className="ad-div comm-box">
       <div>
-        <img src="http://newimg.jspang.com/kaikebaBanner.png" alt="" width="100%" />
-        <img src="http://blogimages.jspang.com/WechatIMG12.jpeg" alt="" width="100%" />
-        <img src="http://newimg.jspang.com/kaikebaBanner.png" alt="" width="100%" />
-        <img src="http://blogimages.jspang.com/WechatIMG12.jpeg" alt="" width="100%" />
+        <img
+          src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1601872162022&di=6f287b7b18ace3bc60952572fade02a4&imgtype=0&src=http%3A%2F%2Fa0.att.hudong.com%2F30%2F29%2F01300000201438121627296084016.jpg"
+          alt=""
+          width="100%"
+        />
+        <img
+          src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3746149156,3846202622&fm=26&gp=0.jpg"
+          alt=""
+          width="100%"
+        />
+        <img
+          src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2099816045,3000912227&fm=26&gp=0.jpg"
+          alt=""
+          width="100%"
+        />
+        <img
+          src="https://image.baidu.com/search/detail?ct=503316480&z=0&ipn=d&word=%E5%9B%BE%E7%89%87&step_word=&hs=2&pn=511&spn=0&di=102850&pi=0&rn=1&tn=baiduimagedetail&is=0%2C0&istype=0&ie=utf-8&oe=utf-8&in=&cl=2&lm=-1&st=undefined&cs=2367033863%2C2519577080&os=790945090%2C192173695&simid=3474062500%2C489379938&adpicid=0&lpn=0&ln=1445&fr=&fmq=1601862064522_R&fm=&ic=undefined&s=undefined&hd=undefined&latest=undefined&copyright=undefined&se=&sme=&tab=0&width=undefined&height=undefined&face=undefined&ist=&jit=&cg=&bdtype=0&oriquery=&objurl=http%3A%2F%2Fa0.att.hudong.com%2F43%2F17%2F01300000400875125022179001625.jpg&fromurl=ippr_z2C%24qAzdH3FAzdH3Fp7rtwg_z%26e3Bkwthj_z%26e3Bv54AzdH3Ftrw1AzdH3Fwa_9n_80_a8naaaaa9aab0c8dcadd80laa8mdc_3r2_z%26e3Bip4s&gsm=200&rpstart=0&rpnum=0&islist=&querylist=&force=undefined"
+          alt=""
+          width="100%"
+        />
       </div>
     </div>
   )
