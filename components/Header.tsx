85922cfd4c11b2aaf145884365a66d7588f6676d
zhouzhihui <peregrinator@yeah.net>
2020-11-06 16:55:26 +0800

    chore: modify the header component style and layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,10 +12,9 @@ import cn from 'classnames'
 import axios from 'axios'
 // import Link from 'next/link'
 import '../public/style/components/header.css'
-import {Row, Col, Menu, Affix, Anchor} from 'antd'
+import {Menu, Anchor} from 'antd'
 import {
   createFromIconfontCN,
-  CaretDownFilled,
   HomeFilled,
   AppstoreFilled,
   GithubFilled,
@@ -30,7 +29,7 @@ const {SubMenu} = Menu
 const {Link} = Anchor
 
 const IconFont = createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/font_2174183_i4qvalcywwp.js',
+  scriptUrl: '//at.alicdn.com/t/font_2174183_loketm9o2m.js',
 })
 
 type Props = {
@@ -68,68 +67,76 @@ const Header = ({indexBG}: Props) => {
 
   return (
     <header className={cn('header', {'full-page': indexBG, 'not-index-bg': !indexBG})} id="header">
-      <nav id="nav">
-        <Affix offsetTop={top} className="header-affix">
-          <Row justify="center" className="header-nav-affix">
-            <Col xs={24} sm={24} md={6} lg={6} xl={6} className="header-logo">
-              <span>MyNetdisk</span>
-            </Col>
-            <Col xs={0} sm={0} md={18} lg={18} xl={18} className="header-menu">
-              <div>
+      <nav id="nav" className="nav">
+        <div className="nav-inner clearfix">
+          <div className="nav-logo">
+            <span>MyNetdisk</span>
+          </div>
+          <div className="nav-menu">
+            <Menu mode="horizontal" onClick={menuClick} triggerSubMenuAction="hover">
+              <Menu.Item>
                 <SearchOutlined />
                 搜索
-              </div>
-              <Menu mode="horizontal" onClick={menuClick} triggerSubMenuAction="hover">
-                <Menu.Item key="index">
-                  <HomeFilled />
-                  主页
+              </Menu.Item>
+              <Menu.Item key="index">
+                <HomeFilled />
+                主页
+              </Menu.Item>
+              <SubMenu
+                key="categories"
+                popupClassName="categories-popup-item popup-item"
+                // onTitleClick={subMenuClick}
+                popupOffset={[0, 0]}
+                icon={<AppstoreFilled />}
+                title="博客">
+                {navArray.map(item => {
+                  return <Menu.Item key={item.Id}>{item.typeName}</Menu.Item>
+                })}
+              </SubMenu>
+              <SubMenu
+                key="hobbies"
+                popupClassName="hobbies-popup-item popup-item"
+                popupOffset={[0, 0]}
+                icon={<IconFont type="aihao" />}
+                title="爱好">
+                <Menu.Item key="music" icon={<IconFont type="music" />}>
+                  音乐
                 </Menu.Item>
-                <SubMenu
-                  key="categories"
-                  popupClassName="popup-item"
-                  // onTitleClick={subMenuClick}
-                  popupOffset={[0, 0]}
-                  icon={<AppstoreFilled />}
-                  title="博客">
-                  {navArray.map(item => {
-                    return <Menu.Item key={item.Id}>{item.typeName}</Menu.Item>
-                  })}
-                </SubMenu>
-                <SubMenu
-                  key="hobbies"
-                  popupClassName="popup-item"
-                  popupOffset={[0, 0]}
-                  icon={<IconFont type="icon-motuoche" />}
-                  title="爱好">
-                  <Menu.Item key="music">音乐</Menu.Item>
-                  <Menu.Item key="movie">电影</Menu.Item>
-                  <Menu.Item key="book">书单</Menu.Item>
-                </SubMenu>
-                <SubMenu
-                  key="links"
-                  popupClassName="popup-item"
-                  popupOffset={[0, 0]}
-                  icon={<IconFont type="icon-link" />}
-                  title="链接">
-                  <Menu.Item key="friend">友链</Menu.Item>
-                  <Menu.Item key="oldblog">老版技博</Menu.Item>
-                </SubMenu>
-                <Menu.Item key="messages">
-                  <IconFont type="icon-msg" />
-                  留言板
+                <Menu.Item key="movie" icon={<IconFont type="movie" />}>
+                  电影
                 </Menu.Item>
-                <Menu.Item key="timeline">
-                  <IconFont type="icon-time" />
-                  时间轴
+                <Menu.Item key="book" icon={<IconFont type="book" />}>
+                  书单
                 </Menu.Item>
-                <Menu.Item key="about">
-                  <IconFont type="icon-account" />
-                  关于我
+              </SubMenu>
+              <SubMenu
+                key="links"
+                popupClassName="links-popup-item popup-item"
+                popupOffset={[0, 0]}
+                icon={<IconFont type="link" />}
+                title="链接">
+                <Menu.Item key="friend" icon={<IconFont type="friend" />}>
+                  友链
                 </Menu.Item>
-              </Menu>
-            </Col>
-          </Row>
-        </Affix>
+                <Menu.Item key="oldblog" icon={<IconFont type="pen" />}>
+                  老版技博
+                </Menu.Item>
+              </SubMenu>
+              <Menu.Item key="messages">
+                <IconFont type="liuyan" />
+                留言板
+              </Menu.Item>
+              <Menu.Item key="timeline">
+                <IconFont type="timeline" />
+                时间轴
+              </Menu.Item>
+              <Menu.Item key="about">
+                <IconFont type="about" />
+                关于我
+              </Menu.Item>
+            </Menu>
+          </div>
+        </div>
       </nav>
       <div className="site-info">
         <h1 id="site-title" className="site-title">
@@ -167,7 +174,7 @@ const Header = ({indexBG}: Props) => {
       <div id="scroll-down" className="scroll-down">
         <Anchor affix={false}>
           <Link title="" href="#anchor">
-            <IconFont type="icon-xiangxiajiantou" className="anticon-down scroll-down-effects" />
+            <IconFont type="downarrow" className="anticon-down scroll-down-effects" />
           </Link>
         </Anchor>
       </div>
