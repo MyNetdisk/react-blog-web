10f5260d44962e977a2d45e770a0e17b5d5f3c12
zhouzhihui <peregrinator@yeah.net>
2020-12-16 16:44:04 +0800

    chore: modify the comment subcomponent to add the comment subcomponent reply

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -2,42 +2,16 @@
 
 import React, {useState} from 'react'
 import {LoadingOutlined} from '@ant-design/icons'
+import Replay from './Reply'
 
 const Comment = () => {
   const [showPanel, setshowPanel] = useState(true)
-  const renderPanel = () => {
-    return (
-      <div className="comment-box">
-        <div id="cancel-comment-reply">
-          <small>
-            <a rel="nofollow" href="/#" id="cancel-comment-reply-link" style={{display: 'none'}}>
-              点击这里取消回复。
-            </a>
-          </small>
-        </div>
-        <form action="/#" method="post" id="conmmentform" className="mobile">
-          <p className="commentator">
-            <input type="text" name="author" id="author" size={22} required placeholder="名称(必须)" />
-            <input type="email" name="email" id="email" size={22} required placeholder="邮箱(必须)" />
-            <input type="url" name="url" id="url" size={22} placeholder="网址(http://)" />
-          </p>
-          <p>
-            <textarea name="comment" id="comment" rows={8} placeholder="填写邮箱可以收到回复哦！" />
-          </p>
-          <p>
-            <input name="submit" type="submit" id="submit" value="提交评论" />
-            <span id="commRem" className="m120" style={{color: '#cd0000'}} />
-          </p>
-        </form>
-      </div>
-    )
-  }
   return (
     <div id="repond">
       <h3 id="comments" className="repond_title">
         发表评论
       </h3>
-      {showPanel ? renderPanel() : ''}
+      <Replay />
       <div className="comment-count">
         <span>88</span>条评论
       </div>
---
+++ components/Reply.tsx
@@ -0,0 +1,33 @@
+/** @format */
+
+import React from 'react'
+
+const Replay = () => {
+  return (
+    <div className="comment-box">
+      <div id="cancel-comment-reply">
+        <small>
+          <a rel="nofollow" href="/#" id="cancel-comment-reply-link" style={{display: 'none'}}>
+            点击这里取消回复。
+          </a>
+        </small>
+      </div>
+      <form action="/#" method="post" id="conmmentform" className="mobile">
+        <p className="commentator">
+          <input type="text" name="author" id="author" size={22} required placeholder="名称(必须)" />
+          <input type="email" name="email" id="email" size={22} required placeholder="邮箱(必须)" />
+          <input type="url" name="url" id="url" size={22} placeholder="网址(http://)" />
+        </p>
+        <p>
+          <textarea name="comment" id="comment" rows={8} placeholder="填写邮箱可以收到回复哦！" />
+        </p>
+        <p>
+          <input name="submit" type="submit" id="submit" value="提交评论" />
+          <span id="commRem" className="m120" style={{color: '#cd0000'}} />
+        </p>
+      </form>
+    </div>
+  )
+}
+
+export default Replay
