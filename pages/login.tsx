9048461303f13f3a5dae16a8cd6844b5e7bba37d
mynetdisk <peregrinator@yeah.net>
2021-03-16 23:42:25 +0800

    chore: modify login and registration

--- pages/login.tsx
+++ pages/login.tsx
@@ -8,16 +8,34 @@ const Login = () => {
   const onFinish = (values: any) => {
     console.log('Received values of form: ', values)
   }
+  
+  const onValuesChange = (values: any) => {
+    console.log('hello', values)
+  }
+
   return (
     <div className="login">
       <div className="login-bg"></div>
       <div className="login-form">
-        <Form layout="vertical" name="normal_login" className="login-form" initialValues={{remember: true}} onFinish={onFinish}>
-          <Form.Item htmlFor="user_name" label="用户名或者电子邮箱号码" name="username" rules={[{required: true, message: 'Please input your Username!'}]}>
-            <Input id="user_name" prefix={<UserOutlined className="site-form-item-icon" />} />
+        <Form
+          layout="vertical"
+          name="normal_login"
+          className="login-form"
+          initialValues={{remember: true}}
+          onFinish={onFinish}
+          onValuesChange={onValuesChange}
+          >
+          <Form.Item
+            label="用户名或者电子邮箱"
+            name="username"
+            rules={[{required: true, message: 'Please input your Username!'}]}>
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} />
           </Form.Item>
-          <Form.Item htmlFor="user_pwd" label="登录密码" name="password" rules={[{required: true, message: 'Please input your Password!'}]}>
-            <Input id="user_pwd" prefix={<LockOutlined className="site-form-item-icon" />} type="password" />
+          <Form.Item
+            label="登录密码"
+            name="password"
+            rules={[{required: true, message: 'Please input your Password!'}]}>
+            <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" />
           </Form.Item>
           <Form.Item>
             <Form.Item name="remember" valuePropName="checked" noStyle>
@@ -33,33 +51,33 @@ const Login = () => {
             <Button type="primary" htmlType="submit" className="login-form-button">
               登录
             </Button>
-            或  <a href="">注册</a>
+            或 <a href="">注册</a>
           </Form.Item>
         </Form>
       </div>
       <style jsx>{`
-        .login-bg{
-            position:absolute;
-            top:0;
-            left:0;
-            width:100%;
-            height:100%;
-            z-index:-1;
-            overflow:hidden;
-            background-image:url('/static/img/bg-wall2.jpg');
-            background-size:cover;
-            background-position:center center;
+        .login-bg {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          z-index: -1;
+          overflow: hidden;
+          background-image: url('/static/img/bg-wall2.jpg');
+          background-size: cover;
+          background-position: center center;
         }
-        .login-form{
-            position:absolute;
-            top:50%;
-            left:50%;
-            width:350px;
-            margin-left:-175px;
-            margin-top:-235px;
-            background-color:#fff;
-            padding:30px;
-            border-radius:8px;
+        .login-form {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          width: 350px;
+          margin-left: -175px;
+          margin-top: -235px;
+          background-color: #fff;
+          padding: 30px;
+          border-radius: 8px;
         }
         #components-form-demo-normal-login .login-form {
           max-width: 300px;
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,55 +1,8 @@
 /** @format */
 
 import React, {useState} from 'react'
-import {Form, Input, Cascader, Select, Row, Col, Checkbox, Button, AutoComplete} from 'antd'
+import {Form, Input, Checkbox, Button} from 'antd'
 
-const {Option} = Select
-
-const residences = [
-  {
-    value: 'zhejiang',
-    label: 'Zhejiang',
-    children: [
-      {
-        value: 'hangzhou',
-        label: 'Hangzhou',
-        children: [
-          {
-            value: 'xihu',
-            label: 'West Lake',
-          },
-        ],
-      },
-    ],
-  },
-  {
-    value: 'jiangsu',
-    label: 'Jiangsu',
-    children: [
-      {
-        value: 'nanjing',
-        label: 'Nanjing',
-        children: [
-          {
-            value: 'zhonghuamen',
-            label: 'Zhong Hua Men',
-          },
-        ],
-      },
-    ],
-  },
-]
-
-const formItemLayout = {
-  labelCol: {
-    xs: {span: 24},
-    sm: {span: 8},
-  },
-  wrapperCol: {
-    xs: {span: 24},
-    sm: {span: 16},
-  },
-}
 const tailFormItemLayout = {
   wrapperCol: {
     xs: {
@@ -70,46 +23,44 @@ const Register = () => {
     console.log('Received values of form: ', values)
   }
 
-  const prefixSelector = (
-    <Form.Item name="prefix" noStyle>
-      <Select style={{width: 70}}>
-        <Option value="86">+86</Option>
-        <Option value="87">+87</Option>
-      </Select>
-    </Form.Item>
-  )
-
-  const [autoCompleteResult, setAutoCompleteResult] = useState<string[]>([])
-
-  const onWebsiteChange = (value: string) => {
-    if (!value) {
-      setAutoCompleteResult([])
-    } else {
-      setAutoCompleteResult(['.com', '.org', '.net'].map(domain => `${value}${domain}`))
+  const onValuesChange = (values: any) => {
+    if(values.nickname){
+      console.log(values.nickname)
     }
+    if(values.email){
+      console.log(values.email)
+    }
+    console.log('hello', values)  
   }
 
-  const websiteOptions = autoCompleteResult.map(website => ({
-    label: website,
-    value: website,
-  }))
+  // const onFieldsChange = (values: any) => {
+  //   console.log('hello', values)
+  // }
+
+
   return (
     <div className="register">
       <div className="register-bg"></div>
       <div className="register-form">
         <Form
-          {...formItemLayout}
+          layout="vertical"
           form={form}
           name="register"
           onFinish={onFinish}
-          initialValues={{
-            residence: ['zhejiang', 'hangzhou', 'xihu'],
-            prefix: '86',
-          }}
+          onValuesChange={onValuesChange}
+          // onFieldsChange={onFieldsChange}
           scrollToFirstError>
           <Form.Item
+            name="nickname"
+            label="用户名"
+            tooltip="What do you want others to call you?"
+            rules={[{required: true, message: 'Please input your nickname!', whitespace: true}]}>
+            <Input />
+          </Form.Item>
+          
+          <Form.Item
             name="email"
-            label="E-mail"
+            label="邮箱"
             rules={[
               {
                 type: 'email',
@@ -125,7 +76,7 @@ const Register = () => {
 
           <Form.Item
             name="password"
-            label="Password"
+            label="密码"
             rules={[
               {
                 required: true,
@@ -138,7 +89,7 @@ const Register = () => {
 
           <Form.Item
             name="confirm"
-            label="Confirm Password"
+            label="确认密码"
             dependencies={['password']}
             hasFeedback
             rules={[
@@ -159,50 +110,6 @@ const Register = () => {
           </Form.Item>
 
           <Form.Item
-            name="nickname"
-            label="Nickname"
-            tooltip="What do you want others to call you?"
-            rules={[{required: true, message: 'Please input your nickname!', whitespace: true}]}>
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            name="residence"
-            label="Habitual Residence"
-            rules={[{type: 'array', required: true, message: 'Please select your habitual residence!'}]}>
-            <Cascader options={residences} />
-          </Form.Item>
-
-          <Form.Item
-            name="phone"
-            label="Phone Number"
-            rules={[{required: true, message: 'Please input your phone number!'}]}>
-            <Input addonBefore={prefixSelector} style={{width: '100%'}} />
-          </Form.Item>
-
-          <Form.Item name="website" label="Website" rules={[{required: true, message: 'Please input website!'}]}>
-            <AutoComplete options={websiteOptions} onChange={onWebsiteChange} placeholder="website">
-              <Input />
-            </AutoComplete>
-          </Form.Item>
-
-          <Form.Item label="Captcha" extra="We must make sure that your are a human.">
-            <Row gutter={8}>
-              <Col span={12}>
-                <Form.Item
-                  name="captcha"
-                  noStyle
-                  rules={[{required: true, message: 'Please input the captcha you got!'}]}>
-                  <Input />
-                </Form.Item>
-              </Col>
-              <Col span={12}>
-                <Button>Get captcha</Button>
-              </Col>
-            </Row>
-          </Form.Item>
-
-          <Form.Item
             name="agreement"
             valuePropName="checked"
             rules={[
@@ -210,15 +117,14 @@ const Register = () => {
                 validator: (_, value) =>
                   value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')),
               },
-            ]}
-            {...tailFormItemLayout}>
+            ]}>
             <Checkbox>
-              I have read the <a href="">agreement</a>
+              我接受 <a href="">协议</a>
             </Checkbox>
           </Form.Item>
           <Form.Item {...tailFormItemLayout}>
             <Button type="primary" htmlType="submit">
-              Register
+              注册
             </Button>
           </Form.Item>
         </Form>
